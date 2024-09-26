e3e4b8398a1fca5450075ad229b1d646e5556eca
tamsynC <tamsyn.m.crangle@student.uts.edu.au>
2024-09-26 18:25:43 +1000

    build files

---
+++ cave_explorer/build/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "cave_explorer"
+PROJECT_SPACE_DIR = "/home/tamsyn2004/catkin_ws/src/cave_explorer/build/devel"
+PROJECT_VERSION = "0.0.1"

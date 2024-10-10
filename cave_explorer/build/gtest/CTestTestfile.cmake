ea9611a009f608bd37db3fdb18f3ad8f34ffa7c3
tamsynC <tamsyn.m.crangle@student.uts.edu.au>
2024-10-10 13:36:39 +1100

    Delete untracked build files

--- cave_explorer/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/tamsyn2004/catkin_ws/src/cave_explorer/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cave_explorer/build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/tamsyn2004/catkin_ws/src/cave_explorer/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
